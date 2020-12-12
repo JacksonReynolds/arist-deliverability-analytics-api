21aade14c4327e7fb73ab3cc799a70e5bedc1c5f
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 17:34:16 -0500

    remove test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/test', to: 'analytics#test'
     post '/register', to: 'analytics#register'
     post '/alive', to: 'analytics#alive'
     post '/report', to: 'analytics#report'
