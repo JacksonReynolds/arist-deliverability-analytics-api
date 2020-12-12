709ebfdcbff6613cf4242405b2b2fddd92031bba
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 17:34:29 -0500

    add attr validations

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,8 @@
 class Report < ApplicationRecord
     belongs_to :device
     validate :device_is_not_disabled
+    validates :message, presence: true
+    validates :sender, presence: true
 
     def device_is_not_disabled
         if self.device && self.device.disabled
