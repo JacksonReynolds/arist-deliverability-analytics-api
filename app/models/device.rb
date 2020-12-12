b1f79a1caa4252c98ec47fa344d1b28882fd1d16
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 14:55:56 -0500

    added bool method to determine active status

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,4 +3,8 @@ class Device < ApplicationRecord
     has_many :reports
     validates :phone_number, phone: true
     validates :carrier, presence: true
+
+    def disabled
+        !!self.disabled_at
+    end
 end
