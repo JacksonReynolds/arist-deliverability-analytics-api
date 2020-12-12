3c31d9ffa837180d42220b1a33fd6ea98399d426
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 01:10:58 -0500

    routes for analytics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/test', to: 'analytics#test'
+    post '/register', to: 'analytics#register'
+    post '/alive', to: 'analytics#alive'
+    post '/report', to: 'analytics#report'
+    patch '/terminate', to: 'analytics#terminate'
   end
 end
