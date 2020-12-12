c12d67fba27c7176ecc0c26465612c817a199839
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 01:11:20 -0500

    stubbed controller actions

--- app/controllers/api/analytics_controller.rb
+++ app/controllers/api/analytics_controller.rb
@@ -2,4 +2,20 @@ class Api::AnalyticsController < ApplicationController
     def test 
         render json: {message: 'success'}, status: 200
     end
+
+    def register
+        
+    end
+
+    def alive
+
+    end
+
+    def report
+
+    end
+
+    def terminate
+
+    end
 end
