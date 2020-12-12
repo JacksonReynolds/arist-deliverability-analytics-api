60e58b598f044722384f45f1788dc04399eabffa
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 14:10:13 -0500

    added invalid phone number error handling

--- app/controllers/api/analytics_controller.rb
+++ app/controllers/api/analytics_controller.rb
@@ -7,9 +7,9 @@ class Api::AnalyticsController < ApplicationController
         # binding.pry
         new_device = Device.new(id: UUID.new.generate, phone_number: params[:phone_number], carrier: params[:carrier])
         if new_device.save
-            render json: {device_id: new_device.id}, status: 200
+            render json: {device_id: new_device.id}, status: 201
         else
-            render json: {}, status: 500
+            render json: {errors: new_device.errors.full_messages}, status: 500
         end
     end
 
