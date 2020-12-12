0b9a67daee238f9c4bf3eb4cf22b1d4da9d88fa5
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 17:16:39 -0500

    disabled device validation

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,10 @@
 class Report < ApplicationRecord
     belongs_to :device
+    validate :device_is_not_disabled
+
+    def device_is_not_disabled
+        if self.device && self.device.disabled
+            self.errors.add(:device, "Device is disabled")
+        end
+    end
 end
