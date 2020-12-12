be7e7647eaa81a991382ca798437b33349fad1cc
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 13:49:59 -0500

    works for valid device data

--- app/controllers/api/analytics_controller.rb
+++ app/controllers/api/analytics_controller.rb
@@ -4,7 +4,13 @@ class Api::AnalyticsController < ApplicationController
     end
 
     def register
-        
+        # binding.pry
+        new_device = Device.new(id: UUID.new.generate, phone_number: params[:phone_number], carrier: params[:carrier])
+        if new_device.save
+            render json: {device_id: new_device.id}, status: 200
+        else
+            render json: {}, status: 500
+        end
     end
 
     def alive
