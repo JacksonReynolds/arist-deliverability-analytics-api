a9fe71f0919cd7f3f5e589ca4a33c55ab9766209
Jackson Reynolds <jwreynold@gmail.com>
2020-12-11 20:12:12 -0500

    only created_at

--- db/migrate/20201212003310_create_heartbeats.rb
+++ db/migrate/20201212003310_create_heartbeats.rb
@@ -2,8 +2,7 @@ class CreateHeartbeats < ActiveRecord::Migration[6.0]
   def change
     create_table :heartbeats do |t|
       t.integer :device_id
-
-      t.timestamps
+      t.datetime :created_at
     end
   end
 end
