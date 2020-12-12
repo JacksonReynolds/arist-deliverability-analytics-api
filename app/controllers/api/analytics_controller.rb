1476ece2b8d27b976153b7d02e4445a82df0ad7f
Jackson Reynolds <jwreynold@gmail.com>
2020-12-12 16:09:14 -0500

    building alive action

--- app/controllers/api/analytics_controller.rb
+++ app/controllers/api/analytics_controller.rb
@@ -1,11 +1,8 @@
 class Api::AnalyticsController < ApplicationController
-    def test 
-        render json: {message: 'success'}, status: 200
-    end
 
     def register
         # binding.pry
-        new_device = Device.new(id: UUID.new.generate, phone_number: params[:phone_number], carrier: params[:carrier])
+        new_device = Device.new(phone_number: params[:phone_number], carrier: params[:carrier])
         if new_device.save
             render json: {device_id: new_device.id}, status: 201
         else
@@ -14,7 +11,18 @@ class Api::AnalyticsController < ApplicationController
     end
 
     def alive
-
+        # binding.pry
+        device = Device.find_by(id: params[:device_id])
+        if device
+            @hb = device.heartbeats.build
+            if @hb.save
+                render json: {}, status: 201
+            else
+                render json: {errors: @hb.errors.full_messages}, status: 500
+            end
+        else
+            render json: {errors: "Device_id invalid"}, status: 500
+        end
     end
 
     def report
