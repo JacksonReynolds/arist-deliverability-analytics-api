4dcfe9eea87ce11d64de61bd3744bcfedd6b9ae3
Jackson Reynolds <jwreynold@gmail.com>
2020-12-20 21:57:47 -0500

    'error' key consistency

--- app/controllers/api/analytics_controller.rb
+++ app/controllers/api/analytics_controller.rb
@@ -1,52 +1,52 @@
 class Api::AnalyticsController < ApplicationController
 
-    def register
-        new_device = Device.new(phone_number: params[:phone_number], carrier: params[:carrier])
-        if new_device.save
-            render json: {device_id: new_device.id}, status: 201
-        else
-            render json: {errors: new_device.errors.full_messages}, status: 500
-        end
+  def register
+    new_device = Device.new(phone_number: params[:phone_number], carrier: params[:carrier])
+    if new_device.save
+      render json: {device_id: new_device.id}, status: 201
+    else
+      render json: {error: new_device.errors.full_messages}, status: 500
     end
+  end
 
-    def alive
-        device = Device.find_by(id: params[:device_id])
-        if device
-            @hb = device.heartbeats.build
-            if @hb.save
-                render json: {}, status: 201
-            else
-                render json: {errors: @hb.errors.full_messages}, status: 500
-            end
-        else
-            render json: {errors: "Device_id invalid"}, status: 500
-        end
+  def alive
+    device = Device.find_by(id: params[:device_id])
+    if device
+      @hb = device.heartbeats.build
+      if @hb.save
+        render json: {}, status: 201
+      else
+        render json: {error: @hb.errors.full_messages}, status: 500
+      end
+    else
+      render json: {error: "Device_id invalid"}, status: 500
     end
+  end
 
-    def report
-        device = Device.find_by(id: params[:device_id])
-        if device
-            @report = device.reports.build(sender: params[:sender], message: params[:message])
-            if @report.save
-                render json: {}, status: 201
-            else
-                render json: {errors: @report.errors.full_messages}, status: 500
-            end
-        else
-            render json: {errors: "Device_id invalid"}, status: 500
-        end
+  def report
+    device = Device.find_by(id: params[:device_id])
+    if device
+      @report = device.reports.build(sender: params[:sender], message: params[:message])
+      if @report.save
+        render json: {}, status: 201
+      else
+        render json: {error: @report.errors.full_messages}, status: 500
+      end
+    else
+      render json: {error: "Device_id invalid"}, status: 500
     end
+  end 
 
-    def terminate
-        @device = Device.find_by(id: params[:device_id])
-        if @device
-            if !@device.disabled? && @device.update(disabled_at: DateTime.now)
-                render json: {}, status: 200
-            else
-                render json: {error: "Device is disabled"}, status: 500
-            end
-        else
-            render json: {error: 'Invalid device_id'}, status: 500
-        end
+  def terminate
+    @device = Device.find_by(id: params[:device_id])
+    if @device
+      if !@device.disabled? && @device.update(disabled_at: DateTime.now)
+        render json: {}, status: 200
+      else
+        render json: {error: "Device is disabled"}, status: 500
+      end
+    else
+      render json: {error: 'Invalid device_id'}, status: 500
     end
+  end
 end
